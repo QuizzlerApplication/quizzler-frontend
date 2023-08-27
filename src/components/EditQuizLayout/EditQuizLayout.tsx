fa61868ad6f9e2547a7e303faeb600600fe4f0af
Anthony Chablov <achablov@myseneca.ca>
2023-08-27 12:43:01 -0400

    build edit layout nav

--- src/components/EditQuizLayout/EditQuizLayout.tsx
+++ src/components/EditQuizLayout/EditQuizLayout.tsx
@@ -1,12 +1,41 @@
+import { useEffect } from "react";
+import useSWR from "swr";
+import { fetchData } from "@/api/quizData";
+import { useParams } from "next/navigation";
 import EditQuizHeader from "./Header/EditQuizHeader"
 import Container from "../Common/Container"
-import useSWR from "swr";
+import { QuizData } from "@/models/quizzes";
 
 const EditQuizLayout = () => {
+
+  /* Next Router */
+  const params = useParams();
+  const quizId = params.quiz;
+
+
+  /* Fetch Data */
+  const { data, error, isValidating , isLoading} = useSWR<QuizData>(
+    `https://quizzlerreactapp.onrender.com/api/quizzes/${quizId}`,
+    fetchData, {
+      revalidateOnFocus: false,
+      refreshInterval: 300000,
+    }
+  );
+
+    useEffect(()=>{
+      console.log(data);
+    },[data]);
+
+  if (isValidating || isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error fetching data</div>;
+  }
   return (
     <>
         <Container>
-            <EditQuizHeader headerText=""/>
+            <EditQuizHeader headerText={data?.quizTitle} quizId={quizId}/>
         </Container>
     </>
   )
--- src/components/EditQuizLayout/Header/EditQuizHeader.tsx
+++ src/components/EditQuizLayout/Header/EditQuizHeader.tsx
@@ -4,24 +4,29 @@ import { useQuizStore } from '@/store/useQuizStore';
 
 interface SingleQuizHeaderProps {
   headerText: string;
+  quizId: string;
 }
 
-const EditQuizHeader: React.FC<SingleQuizHeaderProps> = ({ headerText }) => {
-  /* State */
+const EditQuizHeader: React.FC<SingleQuizHeaderProps> = ({ headerText, quizId }) => {
+  
   
   return (
     <div className='py-4'>
       <div className="flex justify-between items-center">
-        <Link href={'/dashboard'}>
+        <Link href={`/dashboard/quiz/${quizId}`}>
           <p>Start Quiz</p>
         </Link>
         <h1 className='font-bold text-lg'>{headerText}</h1>
-        <button onClick={() => {} }>
-          <Icons type='question' color='#7861f3' size={25}/>
-        </button> 
-        <button onClick={() => {} }>
-          <Icons type='question' color='#7861f3' size={25}/>
-        </button> 
+        <div className=" flex flex-col">
+          <button onClick={() => {} } className='flex'>
+            Delete
+            <Icons type='question' color='#7861f3' size={25}/>
+          </button> 
+          <button onClick={() => {} } className='flex'>
+            Rename
+            <Icons type='question' color='#7861f3' size={25}/>
+          </button> 
+        </div>
       </div>
     </div>
   );
